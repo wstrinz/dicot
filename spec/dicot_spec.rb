6b3d7a70468d823e8b469217d6fd2b95992eb7d6
Will Strinz <wstrinz@gmail.com>
2014-02-21 22:15:09 +0000

    a few more specs

--- spec/dicot_spec.rb
+++ spec/dicot_spec.rb
@@ -4,4 +4,20 @@ describe Dicot do
   it "should label a string" do
     Dicot.label("Hello I am a string").should_not be nil
   end
+
+  it "correctly labels trained string" do
+    str = "Where's Will (Friday morning)"
+    Dicot.label(str).first.map(&:last).should == %w{O O O O B-TS I-TS O}
+  end
+
+
+  it 'identifies features in novel string' do
+    str = "Where's Will (Ragnarok morning)"
+    Dicot.label(str).first.map(&:last).should == %w{O O O O B-TS I-TS O}
+  end
+
+  it "isn't magic" do
+    str = "Where's Will (on the Ragnarok morning)"
+    Dicot.label(str).first.map(&:last).should == %w{O O O O O O O O O}
+  end
 end
